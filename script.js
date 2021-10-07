fcc316a227097cabd78ab673ca565a83aa8559b8
Th3Wall <mandellidavide.95@gmail.com>
2021-10-08 01:59:06 +0200

    WIP - Project setup for styling & first edits

--- script.js
+++ script.js
@@ -4,25 +4,27 @@ let start = document.getElementById('start'),
     preview = document.getElementById('preview'),
     mime = document.getElementById('mimetype-select');
 
-start.addEventListener('click', async function () {
+const appendStatusNotification = actionType => {
+    const notificationText = actionType === "start" ? "Started recording" : actionType === "stop" ? "Stopped recording" : "";
+    let node = document.createElement("p");
+    node.textContent = notificationText;
+    document.body.appendChild(node);
+}
+
+start.addEventListener('click', async () => {
     let stream = await recordScreen();
     let mimeType = 'video/' + mime.value;
-    mediaRecorder = createRecorder(stream, mimeType);
 
+    mediaRecorder = createRecorder(stream, mimeType);
     preview.srcObject = stream;
     preview.captureStream = preview.captureStream || preview.mozCaptureStream;
-
-    let node = document.createElement("p");
-    node.textContent = "Started recording";
-    document.body.appendChild(node);
+    appendStatusNotification("start");
 })
 
-stop.addEventListener('click', function () {
+stop.addEventListener('click', () => {
     mediaRecorder.stop();
     preview.srcObject = null;
-    let node = document.createElement("p");
-    node.textContent = "Stopped recording";
-    document.body.appendChild(node);
+    appendStatusNotification("stop");
 })
 
 async function recordScreen() {
@@ -32,31 +34,31 @@ async function recordScreen() {
     });
 }
 
-function createRecorder(stream, mimeType) {
+const createRecorder = (stream, mimeType) => {
     // the stream data is stored in this array
     let recordedChunks = [];
-
     const mediaRecorder = new MediaRecorder(stream);
 
-    mediaRecorder.ondataavailable = function (e) {
+    mediaRecorder.ondataavailable = (e) => {
         if (e.data.size > 0) {
             recordedChunks.push(e.data);
         }
     };
-    mediaRecorder.onstop = function () {
+    mediaRecorder.onstop = () => {
         saveFile(recordedChunks);
         recordedChunks = [];
     };
+
     mediaRecorder.start(200); // For every 200ms the stream data will be stored in a separate chunk.
     return mediaRecorder;
 }
 
-function saveFile(recordedChunks) {
+const saveFile = (recordedChunks) => {
     const blob = new Blob(recordedChunks, {
         type: 'video/' + mime.value
     });
-    let filename = getRandomString(15);
 
+    let filename = getRandomString(15);
     let downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(blob);
     downloadLink.download = `${filename}.${mime.value}`;
@@ -67,10 +69,10 @@ function saveFile(recordedChunks) {
     document.body.removeChild(downloadLink);
 }
 
-function getRandomString(length) {
-    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var result = '';
-    for ( var i = 0; i < length; i++ ) {
+const getRandomString = (length) => {
+    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for ( let i = 0; i < length; i++ ) {
         result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
     }
     return result;
